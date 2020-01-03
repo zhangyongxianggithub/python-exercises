5bfae5f16b9def822f5caf547371eca006112513
zhangyongxiang <zhangyongxiang@kuaishou.com>
2020-01-03 20:13:41 +0800

    d

--- exercise_1.py
+++ exercise_1.py
@@ -1,9 +1,9 @@
-import os
-print(os.environ)
+l = []
+for num in range(2000, 3201):
+    if num % 7 == 0 and num % 5 != 0:
+        l.append(str(num))
+print(','.join(l))
 
-
-
-
-
-
-
+for num in range(2000, 3201):
+    if num % 7 == 0 and num % 5 != 0:
+        print(num, end=',')
