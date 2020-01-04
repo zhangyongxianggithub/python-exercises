c58417a6a76867dfdcbb2ca1b7123ba2ee2fdf82
zhangyongxiang <zyxfox@foxmail.com>
2020-01-04 22:52:30 +0800

    new dev

--- exercise_1.py
+++ exercise_1.py
@@ -7,3 +7,15 @@ print(','.join(l))
 for num in range(2000, 3201):
     if num % 7 == 0 and num % 5 != 0:
         print(num, end=',')
+
+while True:
+    reply = input('type some world:')
+    if reply == 'stop':
+        break
+    try:
+        int(reply)
+    except:
+        print('Bad input！'*8)
+    else:
+        print(int(reply)**2)
+print('Bye!!!!')
