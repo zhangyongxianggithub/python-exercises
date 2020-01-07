6ee53705dcdcbe524f2145105266b9e1d8902ba8
zhangyongxiang <zhangyongxiang@kuaishou.com>
2020-01-07 19:05:21 +0800

    exercise

--- exercise_2.py
+++ exercise_2.py
@@ -6,3 +6,24 @@ def fac(f):
 
 in_number = input('input the number:')
 print(fac(int(in_number)))
+
+# use while loop
+n = int(input('input a number: '))
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+print(fact)
+#use for loop
+n = int(input('input a number: '))
+fact = 1
+for i in range(1,n+1):
+    fact *= i
+print(fact)
+#use lambda
+n = int(input('input a number: '))
+fact = 1
+def short_fact(x):return 1 if x<=1 else x*short_fact(x-1)
+print(short_fact(n))
+
