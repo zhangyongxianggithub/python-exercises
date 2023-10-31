5b0d65d47d71ecd23e1fd3827f27bd9e51b05cbb
zhangyongxiang <1306835795@qq.com>
2023-10-31 21:40:55 +0800

    update

--- logging/logggg.py
+++ logging/logggg.py
@@ -1,9 +1,10 @@
 import logging
 
+logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s %(process)d --- [%(threadName)s] %(pathname)s(%(lineno)d): %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 
-logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger('test')
+logger.info('ddddd%s', 'aaa')
+logger = logging.getLogger('test1')
+logger.info('ddddd%s', 'aaa')
 
-logger=logging.getLogger('test')
-logger.info('ddddd%s','aaa')
-logger=logging.getLogger('test1')
-logger.info('ddddd%s','aaa')
+print('%(m)s' % {'m': 'xxx'})
