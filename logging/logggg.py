96fb2a276a46a3ecf4aaf84c8821208dd65c82fd
zhangyongxiang <1306835795@qq.com>
2023-10-31 23:15:45 +0800

    update

--- logging/logggg.py
+++ logging/logggg.py
@@ -1,10 +1,21 @@
 import logging
+import sys
+from logging import StreamHandler
+from logging import INFO
+from logging import Handler
+from logging import Logger
+from logging import Formatter
 
-logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s %(process)d --- [%(threadName)s] %(pathname)s(%(lineno)d): %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
-
-logger = logging.getLogger('test')
+logger: Logger = logging.getLogger('clear')
+logger.propagate = 0
+logger.setLevel(INFO)
+handler: Handler = StreamHandler(sys.stdout)
+handler.setLevel(INFO)
+handler.setFormatter(Formatter(
+    fmt='%(asctime)s %(levelname)s %(process)d --- [%(threadName)s] %(pathname)s(%(lineno)d): %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'))
+logger.addHandler(handler)
 logger.info('ddddd%s', 'aaa')
-logger = logging.getLogger('test1')
-logger.info('ddddd%s', 'aaa')
+logger.info('ddddddd%s', 'aaa')
 
 print('%(m)s' % {'m': 'xxx'})
